9440b47865394d561a83d1b2f63655b39ae0c352
doremi31618 <doremi31618@gmail.com>
2022-06-25 09:48:34 +0800

    simplest p5js demo

--- src/javascript/app.js
+++ src/javascript/app.js
@@ -1,12 +1,29 @@
 
+var data = [];
+var width = 400, height = 400;
 function setup() {
     canvas = createCanvas(window.innerWidth, window.innerHeight);
     canvas.position(0, 0);
     canvas.class("p5canvas");
   }
+  
+  function mousePressed(){
+    let x = map(mouseX, 0, window.innerWidth, 0, 1);
+    let y = map(mouseY, 0, window.innerHeight, 1, 0);
+    let point = createVector(x, y);
+    data.push(point);
+  }
 
   function draw() {
     background(0);
+
+    for(let p of data){
+      let x = map(p.x, 0, 1, 0, window.innerWidth);
+      let y = map(p.y, 0, 1, window.innerHeight, 0);
+      fill(255);
+      stroke(255);
+      ellipse(x, y, 8, 8);
+    }
   }
 
   window.onresize = function() {
